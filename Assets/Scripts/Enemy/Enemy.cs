92f562021611669da7c553ef2f8f34c6ef80ce6f
changu0923 <152964087+changu0923@users.noreply.github.com>
2024-01-10 16:55:22 +0900

    Enemy -> MobSlime 추상클래스 변경완료
    UI 추가 완료
    MobSlime 추가완료

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -1,59 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
+using Unity.VisualScripting;    
 using UnityEngine;
 
-public class Enemy : MonoBehaviour
+public abstract class Enemy : MonoBehaviour
 {
-    DamagePopup popup;
-    SpriteRenderer spriteRenderer;
-
     public int currentHP;
     public int maxHP;
-    private float lifeTime = 1f;
 
-    private void Awake()
-    {
-        popup = GetComponent<DamagePopup>();
-        spriteRenderer = GetComponentInChildren<SpriteRenderer>();
-    }
-
-    void Start()
-    {
-        currentHP = maxHP;
-    }
-   
-    public void TakeDamage(float damage)
-    {
-        popup.PrintDamage((int)damage);
-        currentHP -= (int)damage;
-
-        if (currentHP <= 0)
-        {
-            currentHP = 0;
-            Die();
-        }
-    }
-
-    public void Die()
-    {
-        gameObject.tag = "Dead";
-        StartCoroutine("FadeOutAndDestroy");
-    }
-
-    IEnumerator FadeOutAndDestroy()
-    {
-        // 시작 시간
-        float startTime = Time.time;
-
-        while (Time.time < startTime + lifeTime)
-        {          
-            float normalizedTime = (Time.time - startTime) / lifeTime;
-            Color currentColor = spriteRenderer.color;
-            spriteRenderer.color = new Color(currentColor.r, currentColor.g, currentColor.b, 1f - normalizedTime);
-
-            yield return null;
-        }    
-        Destroy(gameObject, 1f);
-    }
+    public abstract void TakeDamage(float damage);
 }
---
+++ Assets/Scripts/Enemy/MobSlime.cs
@@ -0,0 +1,87 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MobSlime : Enemy
+{
+    enum SlimeState
+    {
+        IDLE,
+        MOVE,
+        HIT,
+        DIE,
+    }
+
+    float behaviorTime;
+
+    DamagePopup popup;
+    SpriteRenderer spriteRenderer;
+    private float lifeTime = 1f;
+    SlimeState state;
+
+    private void Awake()
+    {
+        popup = GetComponent<DamagePopup>();
+        spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+    }
+
+    void Start()
+    {
+        maxHP = 50;
+        currentHP = maxHP;
+        state = SlimeState.IDLE;
+    }
+
+    private void Update()
+    {
+        switch (state) 
+        {
+            case SlimeState.IDLE:
+
+                break;
+            case SlimeState.MOVE:
+
+                break;
+            case SlimeState.HIT:
+
+                break;
+            case SlimeState.DIE:
+                break;
+        }
+    }
+
+
+    public override void TakeDamage(float damage)
+    {
+        popup.PrintDamage((int)damage);
+        currentHP -= (int)damage;
+
+        if (currentHP <= 0)
+        {
+            currentHP = 0;
+            Die();
+        }
+    }
+
+    public void Die()
+    {
+        gameObject.tag = "Dead";
+        StartCoroutine("FadeOutAndDestroy");
+    }
+
+    IEnumerator FadeOutAndDestroy()
+    {
+        // 시작 시간
+        float startTime = Time.time;
+
+        while (Time.time < startTime + lifeTime)
+        {
+            float normalizedTime = (Time.time - startTime) / lifeTime;
+            Color currentColor = spriteRenderer.color;
+            spriteRenderer.color = new Color(currentColor.r, currentColor.g, currentColor.b, 1f - normalizedTime);
+
+            yield return null;
+        }
+        Destroy(gameObject, 1f);
+    }
+}
